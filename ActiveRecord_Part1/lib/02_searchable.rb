6db5772c1a4ae20a650b54d099242637fbc06a4f
App_Academy_Student <makerman4@gmail.com>
2019-06-28 17:06:12 -0400

    Create Searchable module to exted SQLOBject class

--- ActiveRecord_Part1/lib/02_searchable.rb
+++ ActiveRecord_Part1/lib/02_searchable.rb
@@ -3,10 +3,20 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.keys.map {|param| "#{param} = ?"}.join(" AND ")
+    table = self.table_name
+    results = DBConnection.execute(<<-SQL, *params.values)
+      SELECT
+        *
+      FROM
+        #{table}
+      WHERE
+        #{where_line}
+    SQL
+    results.map {|result| self.new(result)}
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
